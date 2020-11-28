a263ef499013f8a58b3ca6a48f08dfe8e8d9bc84
StanislawNagorski <stanislaw.nagorski@gmail.com>
2020-11-28 18:25:56 +0100

    doGet in GoogleLogin

--- src/main/java/controllers/users/googleAuth/GoogleLogin.java
+++ src/main/java/controllers/users/googleAuth/GoogleLogin.java
@@ -71,7 +71,7 @@ public class GoogleLogin extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+       doPost(req, resp);
     }
 
 
